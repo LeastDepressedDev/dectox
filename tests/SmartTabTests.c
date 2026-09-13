00041a03425baa8c452889102349653221241826
LeastDepressedDev <zirocilt@gmail.com>
2026-09-14 02:22:55 +0300

    First version of Smart Tab

---
+++ tests/SmartTabTests.c
@@ -0,0 +1,16 @@
+
+#include <stdint.h>
+#include <uchar.h>
+
+
+typedef struct {
+    uint16_t                              ONE; // Initital
+    char                                  TWO; // One tab
+    char16_t                              THREE; // One tab
+    unsigned long long                    FOUR; // One tab
+    char16_t                              FIVE; // Three tabs
+} test_rt;
+
+int main(int argc, char** argv) {
+    return 0;
+}
