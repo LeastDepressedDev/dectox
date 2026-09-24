5c4cdd27c0bca788da62a25be74faf57f6874698
LeastDepressedDev <zirocilt@gmail.com>
2026-09-24 04:42:02 +0300

    Fixed: A bit messed up with rebasing

--- build.sh
+++ build.sh
@@ -10,6 +10,4 @@ if [[ ! $1 = 'offline' ]]; then
 fi
 
 npm run compile --verbose
-cp -r src/assets build/assets
-cp src/left-bar/OptionsProvider.html build/assets/OptionsProvider.html
 vsce package --allow-star-activation --readme-path src/README.md
