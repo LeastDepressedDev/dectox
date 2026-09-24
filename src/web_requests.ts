9a0c52b095f3705c32fb852ce58f65823685046d
LeastDepressedDev <zirocilt@gmail.com>
2026-09-24 09:00:04 +0300

    Javadoc for web_request.ts

--- src/web_requests.ts
+++ src/web_requests.ts
@@ -2,10 +2,26 @@ import * as vsc from 'vscode';
 import * as gbs from './globals'
 import type { Browser, Page } from 'puppeteer-core' with { "resolution-mode": "import" };
 
+/**
+ * Web browser instance. Set when `active` called.
+ */
 export var browser: Browser;
+
+/**
+ * Availability of Web requests functions. 
+ * `true` - brower is up, everything is just fine.
+ * `false` - browser is not up or some error caused a Web requests cutoff.
+ */
 export var available: boolean = false;
+
+/**
+ * Container for pages. Maps id: string to page: Page.
+ */
 export var pages: Map<string, Page>;
 
+/**
+ * Activates Web requests functional. If browser is already available will do nothing.
+ */
 export async function activate() {
     if (available) return;
     const cfg = gbs.configs();
@@ -20,12 +36,26 @@ export async function activate() {
     available = true;
 }
 
+/**
+ * Creates virtual browser tab. 
+ * 
+ * Works like a terminal version of a default browser. It allows you to use cookies and other things. To learn more read `puppeteer` lib docs.
+ * 
+ * @param key `string` id of a tab. 
+ * @returns Promise to the Page instance being initialised. 
+ */
 export async function initTab(key: string): Promise<Page> {
     const page = await browser.newPage();
     pages.set(key, page);
     return page;
 }
 
+/**
+ * Closes virtual browser tab.
+ * 
+ * @param key `string` id of a tab. 
+ * @returns Promise to boolean value. True if operation succeeded.
+ */
 export async function closeTab(key: string): Promise<boolean> {
     if (!pages.has(key)) return false;
     pages.get(key)?.close();
@@ -33,6 +63,9 @@ export async function closeTab(key: string): Promise<boolean> {
     return true;
 }
 
+/**
+ * Deactivates browser functional.
+ */
 export async function close() {
     browser.close();
 }
